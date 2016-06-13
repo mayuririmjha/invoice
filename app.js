5812096ea0c856c08ea2e0864db2622b9be05f78
Mayuri Rimjha <mayuririmjha131@gmail.com>
2016-06-12 23:52:20 -0700

    Reading JSON data set file and displayiing on html

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 //This is the app.js file where all the modules live
 
 var app = angular.module("InvoiceApplication", []);
+
+app.controller('ProductListController', function($scope, $http) {
+  $http.get('data_set.json')
+        .success(function(data) {
+            $scope.products = data;
+        })
+        .error(function(data,status,error,config){
+            $scope.products = [{heading:"Error",description:"Could not load json data"}];
+        });
+
+});
